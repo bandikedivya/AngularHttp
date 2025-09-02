851cd15d98acf6511b11a0d88cdd68f46b118e19
bandikedivya <divyacrkdv@gmail.com>
2025-09-02 16:05:10 +0530

    Http code added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'AngularHttp';
+
+  
+  apiurl = "https://jsonplaceholder.typicode.com/comments";
+  data:any  = []; 
+
+  constructor(private httpclient : HttpClient){
+
+  }
+
+  // used ngonintit to subscribe and to initialize
+  ngOnInit(): void {
+
+    let endpoint  = this.httpclient.get(this.apiurl);
+    endpoint.subscribe((result:any)=>{
+      console.log("The collection of info", result);
+      this.data = result; 
+
+    })
+
+    // this.httpclient.get("https://jsonplaceholder.typicode.com/todos")
+    //                 .subscribe((data)=>{
+    //                   console.log("Data Collection is: ", data);
+    //                 })
+                    
+    
+  }
 }
